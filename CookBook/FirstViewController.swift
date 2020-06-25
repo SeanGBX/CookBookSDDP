6b2c69dae91b9bbd97663d8ccc5ca682f5ad27cd
Arman154khan <arman154khan@gmail.com>
2020-06-25 02:53:48 -0700

    Recreate New Project

---
+++ CookBook/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CookBook
+//
+//  Created by 182558Z  on 6/25/20.
+//  Copyright © 2020 ITP312T3. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CookBook/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CookBook
+//
+//  Created by 182558Z  on 6/25/20.
+//  Copyright © 2020 ITP312T3. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
