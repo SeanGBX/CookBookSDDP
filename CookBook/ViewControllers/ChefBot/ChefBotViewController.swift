3f2e7f49c33b424e6565a67d05e0418b03ef8790
Sean Gwee <seangbx@gmail.com>
2020-06-29 15:59:52 +0800

    Added ChefBot Storyboard and ViewController

---
+++ CookBook/ViewControllers/ChefBot/ChefBotViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ChefBotViewController.swift
+//  CookBook
+//
+//  Created by ITP312Grp2 on 29/6/20.
+//  Copyright © 2020 ITP312T3. All rights reserved.
+//
+
+import UIKit
+import MessageKit
+
+//struct Sender: SenderType {
+//    var senderId: String
+//    var displayName: String
+//}
+//
+//struct Message: MessageType{
+//    var sender: SenderType
+//    var messageId: String
+//    var sentDate: Date
+//    var kind: MessageKind
+//}
+//
+//struct Media: MediaItem {
+//    var url: URL?
+//
+//    var image: UIImage?
+//
+//    var placeholderImage: UIImage
+//
+//    var size: CGSize
+//
+//
+//}
+//class ChefBotViewController: MessagesViewController, MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+//
+//    var friendItem : Friend?
+//
+//    let currentUser = Sender(senderId: "self", displayName: "Me")
+//
+//    var otherUser = Sender(senderId: "other", displayName: "")
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
