40eda14df4096712948a9a5ce434e6a3095baf77
Arman154khan <40318460+Arman154khan@users.noreply.github.com>
2020-07-01 10:41:21 +0800

    Update commentDataManager.swift

--- CookBook/DataManagers/commentDataManager.swift
+++ CookBook/DataManagers/commentDataManager.swift
@@ -14,60 +14,82 @@ class commentDataManager: NSObject {
     
     static let db = Firestore.firestore()
     
-    static func createCommentTable(){
-        
-        SQLiteDB.sharedInstance.execute(sql:
-            "CREATE TABLE IF NOT EXISTS " +
-            "PostComments ( " +
-            "    commentId int primary key autoincrement, " +
-            "    comment text, " +
-            "    username text, " +
-            "    postId int " +
-            ")"
-        )
-        
-    }
-    
-    static func loadComments() -> [PostComment]
-    {
-        let commentRows = SQLiteDB.sharedInstance.query(sql:
-            "SELECT commentId, comment, username, postId " +
-            "FROM PostComments"
-        )
-        
-        var commentList : [PostComment] = []
-        for row in commentRows {
-            commentList.append(
-                PostComment(
-                    postId: row["postId"] as! Int,
-                    comment: row["comment"] as! String,
-                    username: row["username"] as! String,
-                    commentId: row["commentId"] as! Int
-                )
-            )
+    static func loadComments(onComplete: (([PostComment]) -> Void)?){
+        db.collection("postComments").getDocuments(){
+            
+            (querySnapshot, err) in
+            var commentList : [PostComment] = []
+            
+            if let err = err{
+                print("Error getting documents: \(err)")
+            }
+            else{
+                for document in querySnapshot!.documents{
+                    var comment = try? document.data(as: PostComment.self) as! PostComment
+                    
+                    if comment != nil{
+                        commentList.append(comment!)
+                    }
+                }
+            }
+            onComplete?(commentList)
         }
-        return commentList
     }
-    
-    static func insertOrReplaceComment(postComment: PostComment){
-        SQLiteDB.sharedInstance.execute(sql:
-            "INSERT OR REPLACE INTO PostComments (commentId, " +
-            "comment, username, postId) " +
-            "VALUES (?, ?, ?, ?)",
-            parameters: [
-                postComment.commentId,
-                postComment.comment,
-                postComment.username,
-                postComment.postId
-            ]
-        )
+
+    static func storeCommentID(_ ID: String) -> String {
+        var selectedComment =
+            try? db.collection("postComments")
+                .document(ID)
+        var selectedID = selectedComment?.documentID
+        
+        return selectedID!
     }
-    
-    static func deleteComment (postComment: PostComment){
-        SQLiteDB.sharedInstance.execute(sql:
-            "DELETE FROM PostComments WHERE commentId = ?",
-            parameters: [postComment.commentId]
-         )
+
+    static func insertComment(_ comment: PostComment, onComplete: ((String)-> Void)?){
+        var addedDocument = try? db.collection("postComments").addDocument(from: comment, encoder: Firestore.Encoder())
+        
+        comment.commentId = String(addedDocument?.documentID ?? "")
+
+        try? db.collection("postComments")
+            .document(String(comment.commentId))
+            .setData(from: comment, encoder: Firestore.Encoder())
+        {
+            err in
+            
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document successfully added")
+                onComplete?(comment.commentId)
+            }
+        }
+    }
+
+    static func editComment(_ comment: PostComment){
+        try? db.collection("postComments")
+            .document(comment.commentId)
+            .setData(from: comment, encoder: Firestore.Encoder())
+        {
+            err in
+
+            if let err = err {
+                print("Error editing document: \(err)")
+            } else {
+                print("Document successfully edited!")
+            }
+        }
+    }
+
+    static func deleteComment (comment: PostComment){
+        db.collection("postComments").document(comment.commentId).delete() {
+            err in
+
+            if let err = err {
+                print("Error removing document: \(err)")
+            } else {
+                print("Document successfully removed!")
+            }
+        }
     }
 
 }
