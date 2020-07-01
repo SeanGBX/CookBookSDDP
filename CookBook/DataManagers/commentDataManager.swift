5fa3c3905875ec12ffce3b3ac67c73b5b0185c91
Sean Gwee <180570C@mymail.nyp.edu.sg>
2020-06-30 19:50:14 -0700

    Updated ChefBot UI

--- CookBook/DataManagers/commentDataManager.swift
+++ CookBook/DataManagers/commentDataManager.swift
@@ -14,82 +14,82 @@ class commentDataManager: NSObject {
     
     static let db = Firestore.firestore()
     
-    static func loadComments(onComplete: (([PostComment]) -> Void)?){
-        db.collection("postComments").getDocuments(){
-            
-            (querySnapshot, err) in
-            var commentList : [PostComment] = []
-            
-            if let err = err{
-                print("Error getting documents: \(err)")
-            }
-            else{
-                for document in querySnapshot!.documents{
-                    var comment = try? document.data(as: PostComment.self) as! PostComment
-                    
-                    if comment != nil{
-                        commentList.append(comment!)
-                    }
-                }
-            }
-            onComplete?(commentList)
-        }
-    }
-
-    static func storeCommentID(_ ID: String) -> String {
-        var selectedComment =
-            try? db.collection("postComments")
-                .document(ID)
-        var selectedID = selectedComment?.documentID
-        
-        return selectedID!
-    }
-
-    static func insertComment(_ comment: PostComment, onComplete: ((String)-> Void)?){
-        var addedDocument = try? db.collection("postComments").addDocument(from: comment, encoder: Firestore.Encoder())
-        
-        comment.commentId = String(addedDocument?.documentID ?? "")
-
-        try? db.collection("postComments")
-            .document(String(comment.commentId))
-            .setData(from: comment, encoder: Firestore.Encoder())
-        {
-            err in
-            
-            if let err = err {
-                print("Error adding document: \(err)")
-            } else {
-                print("Document successfully added")
-                onComplete?(comment.commentId)
-            }
-        }
-    }
-
-    static func editComment(_ comment: PostComment){
-        try? db.collection("postComments")
-            .document(comment.commentId)
-            .setData(from: comment, encoder: Firestore.Encoder())
-        {
-            err in
-
-            if let err = err {
-                print("Error editing document: \(err)")
-            } else {
-                print("Document successfully edited!")
-            }
-        }
-    }
-
-    static func deleteComment (comment: PostComment){
-        db.collection("postComments").document(comment.commentId).delete() {
-            err in
-
-            if let err = err {
-                print("Error removing document: \(err)")
-            } else {
-                print("Document successfully removed!")
-            }
-        }
-    }
+//    static func loadComments(onComplete: (([PostComment]) -> Void)?){
+//        db.collection("postComments").getDocuments(){
+//            
+//            (querySnapshot, err) in
+//            var commentList : [PostComment] = []
+//            
+//            if let err = err{
+//                print("Error getting documents: \(err)")
+//            }
+//            else{
+//                for document in querySnapshot!.documents{
+//                    var comment = try? document.data(as: PostComment.self) as! PostComment
+//                    
+//                    if comment != nil{
+//                        commentList.append(comment!)
+//                    }
+//                }
+//            }
+//            onComplete?(commentList)
+//        }
+//    }
+//
+//    static func storeCommentID(_ ID: String) -> String {
+//        var selectedComment =
+//            try? db.collection("postComments")
+//                .document(ID)
+//        var selectedID = selectedComment?.documentID
+//        
+//        return selectedID!
+//    }
+//
+//    static func insertComment(_ comment: PostComment, onComplete: ((String)-> Void)?){
+//        var addedDocument = try? db.collection("postComments").addDocument(from: comment, encoder: Firestore.Encoder())
+//        
+//        comment.commentId = String(addedDocument?.documentID ?? "")
+//
+//        try? db.collection("postComments")
+//            .document(String(comment.commentId))
+//            .setData(from: comment, encoder: Firestore.Encoder())
+//        {
+//            err in
+//            
+//            if let err = err {
+//                print("Error adding document: \(err)")
+//            } else {
+//                print("Document successfully added")
+//                onComplete?(comment.commentId)
+//            }
+//        }
+//    }
+//
+//    static func editComment(_ comment: PostComment){
+//        try? db.collection("postComments")
+//            .document(comment.commentId)
+//            .setData(from: comment, encoder: Firestore.Encoder())
+//        {
+//            err in
+//
+//            if let err = err {
+//                print("Error editing document: \(err)")
+//            } else {
+//                print("Document successfully edited!")
+//            }
+//        }
+//    }
+//
+//    static func deleteComment (comment: PostComment){
+//        db.collection("postComments").document(comment.commentId).delete() {
+//            err in
+//
+//            if let err = err {
+//                print("Error removing document: \(err)")
+//            } else {
+//                print("Document successfully removed!")
+//            }
+//        }
+//    }
 
 }
--- CookBook/ViewControllers/ChefBot/ChefBotViewController.swift
+++ CookBook/ViewControllers/ChefBot/ChefBotViewController.swift
@@ -9,52 +9,61 @@
 import UIKit
 import MessageKit
 
-//struct Sender: SenderType {
-//    var senderId: String
-//    var displayName: String
-//}
-//
-//struct Message: MessageType{
-//    var sender: SenderType
-//    var messageId: String
-//    var sentDate: Date
-//    var kind: MessageKind
-//}
-//
-//struct Media: MediaItem {
-//    var url: URL?
-//
-//    var image: UIImage?
-//
-//    var placeholderImage: UIImage
-//
-//    var size: CGSize
-//
-//
-//}
-//class ChefBotViewController: MessagesViewController, MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
-//
-//    var friendItem : Friend?
-//
-//    let currentUser = Sender(senderId: "self", displayName: "Me")
-//
-//    var otherUser = Sender(senderId: "other", displayName: "")
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
+class ChefBotViewController: MessagesViewController, MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+
+    var friendItem : Friend?
+
+    let currentUser = Sender(senderId: "self", displayName: "Me")
+
+    var otherUser = Sender(senderId: "other", displayName: "Other")
+    
+    var messages = [MessageType]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        messages.append(Message(
+           sender: otherUser,
+           messageId: "1",
+           sentDate: Date().addingTimeInterval(-70400),
+           kind: .text("Hi! You can ask me any question related to cooking or to our app and i will try my best to answer!")
+       ))
+        messages.append(Message(
+            sender: currentUser,
+            messageId: "2",
+            sentDate: Date().addingTimeInterval(-60400),
+            kind: .text("How many cups in a gallon?")
+        ))
+        messages.append(Message(
+            sender: otherUser,
+            messageId: "3",
+            sentDate: Date().addingTimeInterval(-50400),
+            kind: .text("There are 16 cups in 1 gallon")
+        ))
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
+    
+    func currentSender() -> SenderType {
+        return currentUser
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
